e5389aa0fae15ef303ff33a0db9a69ea59418d24
Miraj Mahmood <mirajmahmood@Mirajs-MacBook-Air.local>
2015-10-21 20:08:32 +0300

    webform added with css

--- forms.py
+++ forms.py
@@ -1,3 +1,13 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import StringField, IntegerField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired
+
+
+
+class classifyForm(Form):
+	sentence = TextAreaField("Sentence :", validators=[DataRequired()])
+	score = StringField("Score :")
+	submit = SubmitField("Classify")
+
+class feedbackForm(Form):
+	feedback_classification = StringField()
--- views.py
+++ views.py
@@ -1,9 +1,15 @@
 from _init_ import *
 from forms import *
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-	return "Hello World"
+	form = classifyForm()
+	if request.method =='POST':
+		if form.validate_on_submit():
+			form.score.data = "score!"
+			feedback_form = feedbackForm()
+			return render_template("home.html", form=form, feedback_form=feedback_form)
+	return render_template("home.html", form=form)
 
 @app.route('/test/') #/test/ will lead urls /test & /test/
 def test():
