c58d4cd1144db3adb16c380b7a254fc82c44e96f
Miraj Mahmood <mirajmahmood@Mirajs-MacBook-Air.local>
2015-11-06 19:45:26 +0300

    feedback is being recorded now

--- views.py
+++ views.py
@@ -13,17 +13,34 @@ def home_page():
 		if form.validate_on_submit():
 			class_ = classify(str(form.sentence.data.lower()), RNN)
 			if class_ is not None:
-				if class_[0]>class_[1]:
+				if class_[0]-class_[1] <0.21:
+					form.score.data = "Neutral"
+				elif class_[0]>class_[1]:
 					form.score.data = "Impolite"
-					return render_template("home.html", form=form, feedback_form=feedback_form, color='#e33100')
 				else:
 					form.score.data = "Polite"
-					return render_template("home.html", form=form, feedback_form=feedback_form, color='#0CB40C')
-			else:
-				form.score.data = "error"
-			return render_template("home.html", form=form, feedback_form=feedback_form, color='#40b3ff')
+			
+			return render_template("home.html", form=form, feedback_form=feedback_form)
 	return render_template("home.html", form=form, feedback_form=None, color='#40b3ff')
 
-@app.route('/test/') #/test/ will lead urls /test & /test/
+
+@app.route('/feedback/', methods=['GET', 'POST'])
 def feedback():
+
+	text = request.form['sentence']
+	label = 0
+	if "polite_button" in request.form:
+		label = 1
+	elif "impolite_button" in request.form:
+		label = 0
+	elif "neutral_button" in request.form:
+		label = 0.5
+		
+	else:
+		return "nothing"
+
+	record_feedback(text, label, writer)
+	return "Thank you for your feedback"
+@app.route('/test/') #/test/ will lead urls /test & /test/
+def test():
 	return render_template("test.html")
