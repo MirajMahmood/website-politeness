06c0309fc19386fcb6e9168585c8323acfde44dc
Miraj Mahmood <mirajmahmood@Mirajs-MacBook-Air.local>
2015-11-14 18:15:42 +0300

    checking something

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from _init_ import *
 
 
 def run_java():
-	#os.system("javac -classpath './WebBackendParser/src:./WebBackendParser/src/stanford-corenlp-full/*:./WebBackendParser/src/py4j0.9.jar:' WebBackendParser/src/TreeMaker.java")
+	os.system("javac -classpath './WebBackendParser/src:./WebBackendParser/src/stanford-corenlp-full/*:./WebBackendParser/src/py4j0.9.jar:' WebBackendParser/src/TreeMaker.java")
 	os.system("java -cp ':./WebBackendParser/src/:./WebBackendParser/src/stanford-corenlp-full/*:./WebBackendParser/src/py4j0.9.jar:' TreeMaker")
 
 
